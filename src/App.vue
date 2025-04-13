8c670849d7488cd9e3b4ac369890578686be7941
DeepDarkBoy48 <warghost200818@gmail.com>
2025-04-13 13:09:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app">
+    <h1>你好啊！</h1>
+  </div>
+</template>
+
+<script lang="ts">
+console.log("App组件被创建了");
+console.log("App组件被创建了");
+export default {
+  name: "Appxuzi", //组件名
+  created() {
+    // 组件创建完成时执行的生命周期钩子函数
+    console.log("App组件被创建了"); // 添加 console.log
+    const message = "Hello Vue!";
+    console.log("message:", message); // 打印变量值
+  },
+};
+</script>
+
+<style>
+.app {
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+// 1. 导入vue
+import {createApp} from 'vue'
+// 2. 导入App组件
+import App from './App.vue'
+// 3. 创建vue实例
+createApp(App).mount('#app')
+console.log(App.name)
+console.log(App.name+'xuzi')
